3f0575ccd85e0c5b7d2409c30b4b33530aee03d5
Isaacdevs <imthimun@student.wethinkcode.co.za>
2022-07-27 14:17:11 +0200

    Latest version of the Web app

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -132,7 +132,7 @@ def main():
             )	
 
         st.markdown("<h2 style='text-align: center; color: orange;'>About The Platform</h2>", unsafe_allow_html=True)
-        st.image('resources/images/bot.webp')
+        st.image('resources/images/recommender_bot.jpg')
         st.markdown("<p style='text-align: left; color: white;'>In today’s technology driven world, recommender systems are socially and economically critical to ensure that individuals can make optimised choices surrounding the content they engage with on a daily basis. One application where this is especially true is movie recommendations; where intelligent algorithms can help viewers find great titles from tens of thousands of options.</p>", unsafe_allow_html=True)
         st.write(
                 """
